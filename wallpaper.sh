88d4f73b2df9fbf8c880f5a97b9a1ae0c13ebb67
gengbeilei <fosky@fosky-archlinux.localdomain>
2020-04-06 12:23:22 +0800

    org

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
-wpnames=(`ls ~/图片/wallpapers/*.jpg`)
+wpnames=(`ls ~/Pictures/wallpapers/*.jpg`)
 N=${#wpnames[@]}
 wp=$[RANDOM%$N+1]
-feh --bg-scale ${wpnames[$wp]}
+feh --bg-fill ${wpnames[$wp]}
 
